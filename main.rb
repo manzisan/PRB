0c3106bf0ce937d8d640aa8da0ee00e91800f4bb
naoki <manzisann@gmail.com>
2017-10-18 21:27:30 +0900

    Update puts data

--- main.rb
+++ main.rb
@@ -1,9 +1,21 @@
 require 'discordrb'
- 
-bot = Discordrb::Bot.new token: 'MzcwMDMyMzkyMTU4NTc2NjQw.DMikxQ.gIgLIDHf9r3qH6UTxE7mfaMWelk', client_id: 370032392158576640
- 
-bot.message do |event|
-    event.respond 'Hello, world!'
+
+bot = Discordrb::Bot.new token: 'MzcwMDMyMzkyMTU4NTc2NjQw.DMjHhg.qmoKjIrGvinDEXBg8AJilMvyLmM', client_id: 370032392158576640
+
+ # for echo
+ # event.respond METHOD
+
+bot.message do |bm|
+  p "------------------------------------"
+  p ("ユーザー名 : #{bm.user.name}")
+  p ("ユーザーID : #{bm.user.id}")
+  p ("メッセージ : #{bm.content}")
+  p "------------------------------------"
+  if bm.user.id === 351323974703251456
+    bm.respond "かつやは死んでね"
+  end
 end
 
 bot.run
+
+# https://discordapp.com/oauth2/authorize?&client_id=370032392158576640&scope=bot&permissions=0
