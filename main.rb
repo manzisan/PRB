1e6c33311ee6c6b363803b02f037cfbe7f46abd0
naoki <manzisann@gmail.com>
2017-10-19 15:34:26 +0900

    Fix pubg status log

--- main.rb
+++ main.rb
@@ -3,14 +3,12 @@ require 'date'
 require 'twitter'
 require 'net/ping'
 
-# for Token
-require '/token'
+# for Token's
+require './token'
 
-# Discord BOT Token
-bot = Discordrb::Bot.new token: 'MzcwMDMyMzkyMTU4NTc2NjQw.DMjHhg.qmoKjIrGvinDEXBg8AJilMvyLmM', client_id: 370032392158576640
+# Discord BOT
+bot = Discordrb::Bot.new token: @d_token, client_id: @id
 
- # for echo
- # event.respond METHOD
 
 bot.message do |bm|
   # *----------  ログとして出力  ----------*/
@@ -25,37 +23,12 @@ bot.message do |bm|
   #   f.puts "------------------------------------"
   # end
 
-  # メッセージを送ったユーザーのデータを取得
-  p "------------------------------------"
-  p ("ユーザー名 : #{bm.user.name}")
-  p ("ユーザーID : #{bm.user.id}")
-  p ("メッセージ : #{bm.content}")
-  p ("タイムスタンプ : #{bm.timestamp.localtime}")
-  p "------------------------------------"
-
-
-  # 特定のユーザーにコメントを送り返す
-  if bm.user.id === 351323974703251456 #かつや
-    bm.respond "かつくんって呼ばれると勃ちます"
-  elsif bm.user.id === 325812579392028674 #ぶっち
-    bm.respond "ぶっち童貞捨てた？"
-  elsif bm.user.id === 269453299743195137 #おかっち
-    bm.respond "叙々苑!?"
-  elsif bm.user.id === 308880612914233344 #しおん
-    bm.respond "小森　純です"
-  elsif bm.user.id === 355359958415704065 #なるさま
-    bm.respond "またスケベしてきたのか"
-  end
-end
-
-def PUBGstatus
-  bot.message do |bm|
-
+  if bm.content === "pubg"
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = 'l1DSd1hSKgIroTZRTEVqdw0xY'
-      config.consumer_secret = 'AfMZKoxS9BSxI1doYSQuTv0meWYI7Tb4CRKx2OA432O94GAOti'
-      config.access_token    = '1217512579-nPNzvgTqigbVMeHaVktRTWg5uOE8ABAE6CB7kLM'
-      config.access_token_secret = 's8Q07LeOjGbN2FppWvfwq8zFkFTyJFMf6pVtq8mg6HLPE'
+      config.consumer_key    = @key
+      config.consumer_secret = @secret_key
+      config.access_token    = @t_token
+      config.access_token_secret = @secret_token
     end
 
     # *----------  PUBGのtimeline取得  ----------*/
@@ -68,15 +41,41 @@ def PUBGstatus
     addr = "13.112.63.251"
     pinger = Net::Ping::External.new(addr)
 
+    time = Time.now.localtime.strftime("%m月 %d日 %H:%M:%S")
+
     # *----------  Pingテスト  ----------*/
     if pinger.ping?
-      bm.send_message ("#{Time.now}時、PUBGサーバーは正常稼動中です。")
+      # bm.send_message ("#{time}現在、PUBGサーバーは正常稼動中です。")
+      bm.send_message ("#{time}現在、PUBGサーバーは正常稼動中です。")
     else
-      bm.send_message ("#{Time.now}時、サーバーがダウンしています")
+      bm.send_message ("#{time}現在、サーバーがダウンしています")
     end
+  end 
+  
+  # メッセージを送ったユーザーのデータを取得
+  p "------------------------------------"
+  p ("ユーザー名 : #{bm.user.name}")
+  p ("ユーザーID : #{bm.user.id}")
+  p ("メッセージ : #{bm.content}")
+  p ("タイムスタンプ : #{bm.timestamp.localtime.strftime("%m月 %d日 %H:%M:%S")}")
+  p "------------------------------------"
+
+  if bm.content === "かつや"
+    bm.send_message "PUBG下手くそな人だ！"
+  end
+
+  # 特定のユーザーにコメントを送り返す
+  if bm.user.id === 351323974703251456 #かつや
+    # bm.respond "かつくんって呼ばれると勃ちます"
+  elsif bm.user.id === 325812579392028674 #ぶっち
+    # bm.respond "ぶっち童貞捨てた？"
+  elsif bm.user.id === 269453299743195137 #おかっち
+    # bm.respond "叙々苑!?"
+  elsif bm.user.id === 308880612914233344 #しおん
+    # bm.respond "小森　純です"
+  elsif bm.user.id === 355359958415704065 #なるさま
+    # bm.respond "またスケベしてきたのか"
   end
 end
 
 bot.run
-PUBGstatus
-# https://discordapp.com/oauth2/authorize?&client_id=370032392158576640&scope=bot&permissions=0
