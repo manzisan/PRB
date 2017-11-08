7bce3762d443134deea86d6acb0f07c1f653dd64
naoki <manzisann@gmail.com>
2017-11-08 10:56:46 +0900

    Update pubg command

--- prb.rb
+++ prb.rb
@@ -11,23 +11,16 @@ bot = Discordrb::Bot.new token: @d_token, client_id: @id
 # Keep current tweet
 current = ""
 
-# for ping
-addr = "13.112.63.251"
-pinger = Net::Ping::External.new(addr)
-time = Time.now.localtime.strftime("%m月 %d日 %H:%M:%S")
+# for send to pinger result
+ping_result = ""
 
+# for ping
+addr = ["13.112.63.251","46.51.255.254","52.68.63.252","52.192.63.252","52.196.63.252","54.64.0.2","54.92.0.2","54.95.0.2"]
+
+time = Time.now.localtime.strftime("%m月 %d日 %H:%M:%S")
+# inger = Net::Ping::External.new(addr)
 # get message
 bot.message do |bm|
-
-    # *----------  Puts log  ----------*/
-    File.chmod(0777, "chat-log.txt")
-    File.open("chat-log.txt", "a") do |f|
-      f.puts "------------------------------------"
-      f.puts ("ユーザー名 : #{bm.user.name}")
-      f.puts ("メッセージ : #{bm.content}")
-      f.puts ("時刻 : #{bm.timestamp.localtime.strftime("%m月 %d日 %H:%M:%S")}")
-      f.puts "------------------------------------"
-    end
 
     # *----------  Puts PUBG status  ----------*/
     if bm.content === "pubg"
@@ -48,40 +41,37 @@ bot.message do |bm|
         current = @client.status(timeline.id).text
       end
 
-      # *----------  Ping  ----------*/
-      if pinger.ping?
-        bm.send_message ("#{time}現在、PUBGサーバーは正常稼動中です。")
-      else
-        bm.send_message ("#{time}現在、サーバーがダウンしています")
+      addr.each_with_index do |p,index|
+        pinger = Net::Ping::External.new(addr[index])
+        if pinger.ping?
+          ping_result = "#{time}現在、#{index+1}番目のサーバーがしんでるよ!"
+        else
+          if index == addr.length
+            ping_result = "#{time}現在、すべてのサーバーは動いてるよ!"
+          end
+        end
       end
+
+      bm.send_message ping_result
+
     end
 
     # *----------  Regularly  ----------*/
+    i = 0
     if bm.content === "watch"
       while true do
-        flag = 0
+        pinger = Net::Ping::External.new(addr[i])
         # *----------  Ping  ----------*/
         if pinger.ping?
           bm.send_message ("#{time}現在、PUBGサーバーは正常稼動中です。")
         else
           bm.send_message ("#{time}現在、サーバーがダウンしています")
         end
+        i++
         sleep(7600)
       end
     end
 
-    # Reply for specific user
-    if bm.user.id === 351323974703251456 #かつや
-      # bm.send_message ""
-    elsif bm.user.id === 325812579392028674 #ぶっち
-      # bm.send_message "うるせぐすな"
-    elsif bm.user.id === 269453299743195137 #おかっち
-      # bm.send_message "叙々苑!?"
-    elsif bm.user.id === 308880612914233344 #しおん
-      # bm.send_message ""
-    elsif bm.user.id === 355359958415704065 #なるさま
-      bm.send_message "またスケベしてきたのか"
-    end
 end
 
 bot.run
